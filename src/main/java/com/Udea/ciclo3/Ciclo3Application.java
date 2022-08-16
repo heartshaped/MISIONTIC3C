ab08bf2c68c93979f5a3aac5adb626bf1788fd70
shiv <42521900+heartshaped@users.noreply.github.com>
2022-08-16 08:08:25 -0500

    Hola Mundo!

--- src/main/java/com/Udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -2,12 +2,23 @@ package com.Udea.ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola :P";
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+		System.out.println("Hola Mundo");
 	}
 
 }
