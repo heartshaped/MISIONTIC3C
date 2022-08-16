b34fac072fa8d2ee4d16ef7f9a75b5e5b8d35e3b
Cyclonesoft <faltdorr@hotmail.com>
2022-08-16 09:55:29 -0500

    conexion_postgress
    conexion pstgress y primer helloword en html

--- src/main/java/com/Udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -2,13 +2,23 @@ package com.Udea.ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@RestController
+
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello (){
+		return "Sobreviviendo a java";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
-		System.out.println("hello word");
+
 	}
 
 }
