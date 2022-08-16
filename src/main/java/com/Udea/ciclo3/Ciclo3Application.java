4d4d3896c7e8e7a59bbdd08a8a758266dcec7fef
Cyclonesoft <faltdorr@hotmail.com>
2022-08-16 07:17:15 -0500

    prueba
    mandando datos de calse

---
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -0,0 +1,13 @@
+package com.Udea.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Udea/ciclo3/Ciclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Udea.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
