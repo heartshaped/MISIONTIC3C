43d11b440330dc79d03cb81857e25b76b69608c0
Angie0601 <anyi.lore@hotmail.com>
2022-08-19 08:26:20 -0500

    Primer Commit

--- src/main/java/com/Udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -2,9 +2,18 @@ package com.UdeA.ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/Hello")
+	public String hello(){
+		return "Hola Ciclo 3 ... saldremos vivos de esto!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
